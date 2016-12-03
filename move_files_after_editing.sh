6f5837d45f1db8085e6dfe34fb45229aef65caa4
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-12-03 17:02:51 -0500

    Update move files script

--- move_files_after_editing.sh
+++ move_files_after_editing.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #  move_files_after_editing.sh
-#  
+#
 #
 #  Created by John Mulhausen on 10/17/13.
 #
@@ -10,6 +10,5 @@ cp -fr batcache/advanced-cache.php wordpress/wp-content/
 cp -fr batcache/batcache.php wordpress/wp-content/plugins/
 cp -fr wp-memcache/object-cache.php wordpress/wp-content/
 cp -fr appengine-wordpress-plugin/. wordpress/wp-content/plugins/
-rm -rf batcache/
-rm -rf wp-memcache/
-rm -rf appengine-wordpress-plugin/
+cp -fr blog wordpress/wp-content/themes/.
+cp -fr batcache/batcache.php wordpress/wp-content/plugins/
