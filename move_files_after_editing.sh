e11fea81923350cc955174915604ba6ca5700a03
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-12-03 19:46:22 -0500

    Fix move_files_after_editing.sh

--- move_files_after_editing.sh
+++ move_files_after_editing.sh
@@ -5,10 +5,11 @@
 #
 #  Created by John Mulhausen on 10/17/13.
 #
+cp -fr modules/wordpress/* wordpress/
 cp -fr wp-config.php wordpress/
-cp -fr batcache/advanced-cache.php wordpress/wp-content/
-cp -fr batcache/batcache.php wordpress/wp-content/plugins/
-cp -fr wp-memcache/object-cache.php wordpress/wp-content/
-cp -fr appengine-wordpress-plugin/. wordpress/wp-content/plugins/
-cp -fr blog wordpress/wp-content/themes/.
-cp -fr batcache/batcache.php wordpress/wp-content/plugins/
+cp -fr modules/batcache/advanced-cache.php wordpress/wp-content/
+cp -fr modules/batcache/batcache.php wordpress/wp-content/plugins/
+cp -fr modules/wp-memcache/object-cache.php wordpress/wp-content/
+cp -fr modules/appengine-wordpress-plugin/. wordpress/wp-content/plugins/
+cp -fr modules/wordpress-seo wordpress/wp-content/plugins/
+cp -fr modules/blog wordpress/wp-content/themes/.
